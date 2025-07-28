07a7a6da40bbcafb0b5cd1a164f8f6166b90ddc1
Sajjad Hossain <sajjadhossain44371@gmail.com>
2025-07-28 17:01:41 +0600

    completed ternary task

---
+++ ternary.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 20;
+let result;
+
+// if(num1 > num2) {
+//     result = num1 * 2;
+//     console.log("result is: ", result);
+// } else {
+//     result = num1 + num2;
+//     console.log("result is: ",result);
+// }
+
+//using ternary
+num1 > num2 ? console.log("result is:", result = num1 * 2) : console.log("result is:", result = num1 + num2);
